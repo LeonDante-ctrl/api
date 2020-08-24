cc1fbd90b5c789e709b7c9f8f56883168b2c0a85
Dante.L <dantel@D.L>
2020-08-24 11:33:11 +0300

    inserted import function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import{Quote}from'./quote';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quotes';
+}
+quotes=[
+   new Quote(1, 'EAT YOUR FRUITS'),
+   new Quote(2, 'I dont know about you , but im dealt with.'),
+   new Quote(3, 'follow through wit your plans'),
+ ]
 }
