eb9ed27311812a177902da2355944dfd3bca4951
Dante.L <dantel@D.L>
2020-08-24 12:08:06 +0300

    added targets to set styling on ,ater on in the html pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -16,28 +17,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'quotes'`, () => {
+  it(`should have as title 'Votes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('quotes');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('Votes');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('quotes app is running!');
-  });
-});
-
-import { Quote } from './quote';
-
-describe('Quote', () => {
-  it('should create an instance', () => {
-    expect(new Quote()).toBeTruthy();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Votes!');
   });
 });
